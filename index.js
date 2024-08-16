3459cffb82e7c5b5e3a7e613e469e3f1df21e812
oweeeenie <owensullivan419@yahoo.com>
2024-08-16 19:28:38 -0400

    troubleshooting DEL and operators not working.

--- index.js
+++ index.js
@@ -1,8 +1,3 @@
-let firstNumber = 6;
-let operator = "+";
-let secondNumber = 9;
-
-
 function add(numOne , numTwo) {
     return numOne + numTwo;
 }
@@ -16,6 +11,7 @@ function multiply(numOne , numTwo) {
 }
 
 function divide(numOne , numTwo) {
+    if (numTwo === 0) return "error"
     return numOne / numTwo;
 }
 
@@ -33,3 +29,60 @@ function operate(operator , numOne , numTwo) {
             return "Invalid Operator";
     }
 }
+
+const numberButtons = document.querySelectorAll(".btn:not(.symbol)");       //this is super cool, :not makes it so it only chooses THE NUMBERS/decimal. not symbols.
+const operatorButtons = document.querySelectorAll(".btn.symbol");
+const equalsButton = document.querySelector("#equals");
+const clearButton = document.querySelector("#clear");
+const deleteButton = document.querySelector("#delete");
+let displayContent = "";
+let firstNumber = ""
+let operator = ""
+let secondNumber = ""
+
+function updateDisplay() {
+    document.querySelector("#display").textContent = displayContent;
+}
+
+numberButtons.forEach(button => {
+    button.addEventListener("click" , (e) => {          // (e) is a paramater. you can use whatever u want.
+        if (e.target.textContent === "." && displayContent.includes(".")) return;
+        displayContent += e.target.textContent;
+        updateDisplay();
+    });
+});
+
+operatorButtons.forEach(button => {
+    button.addEventListener("click" , (e) => {
+        if (displayContent === "") return;
+        firstNumber = displayContent;
+        operator = e.target.textContent;
+        displayContent = "";
+        }); 
+});
+
+equalsButton.addEventListener("click" , () => {
+    if (displayContent === "" || firstNumber === "" || operator === "") return; // this is a guard, to make sure theres actual buttons clicked. doent execute below if no buttons are pressed.
+    secondNumber = displayContent;
+    const result = operate(operator , parseFloat(firstNumber) , parseFloat(secondNumber));          // parsefloat convert strings to numbers.
+    displayContent = result.toString();
+    updateDisplay();
+    firstNumber = "";           //the next 3 lines reset it for the calculcation.
+    operator = "";
+    secondNumber = "";
+});
+
+clearButton.addEventListener("click" , () => {
+    displayContent = "";
+    firstNumber = "";
+    operator = "";
+    secondNumber = "";
+    updateDisplay();
+});
+
+deleteButton.addEventListener("click" , () => {
+    if (displayContent.length > 0) {
+        displayContent = displayContent.slice(0, -1);
+        updateDisplay();
+    }
+});
